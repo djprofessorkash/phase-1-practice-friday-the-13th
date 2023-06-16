89e6f7a117094e991f97f46d51722a81aafbbff1
Aakash Sudhakar <aakash.sudhakar@gmail.com>
2023-06-16 15:01:31 -0400

    [UPDATE] Documented Solution Code for Challenge #1

--- src/index.js
+++ src/index.js
@@ -1,29 +1,43 @@
-/*
-GLOBAL VARIABLE DECLARATIONS
-*/
-let movieData;
-let currentMovie;
-
 /* 
-SOLUTION TO CHALLENGE #1:
-LOADING IMAGES INTO FRONTEND.
+👁️‍🗨️ SOLUTION TO CHALLENGE #1:
+🎞️ LOADING IMAGES INTO FRONTEND.
 */
+
+/*
+📝 PSEUDOCODE FOR CHALLENGE #1:
+    • Fetch data from JSON database object. 🐕
+    • Then, JSONify the data. 🔎
+    • Then, iterate through movies (data) and render each image in HTML. 🎥
+    • To render each movie... 🎬
+        -> Select region on HTML for loading movie images (#movie-list). ⽥
+        -> Create an element for each image. ⚽️
+        -> Update the image link (src) for each image. 🔗
+        -> Add/append the updated image element to the image-loading region. 🖇️
+*/
+
+// Fetch data from JSON database object
 fetch("http://localhost:3000/movies")
+    // JSONify the data
     .then(response => response.json())
-    .then(json => {
-        movieData = json;
-        movieData.forEach(movie => {
+    // Iterate through movies and render to HTML
+    .then(movies => {
+        movies.forEach(movie => {
             renderMovieImageInBar(movie);
         })
     })
 
 /*
-HELPER FUNCTIONS
+HELPER FUNCTIONS:
+RENDERING MOVIE IMAGES IN THE NAV-BAR
 */
-const renderMovieImageInBar = (movie) => {
+const renderMovieImageInBar = (movie) =>{
+    // Track region for loading movie images
     let movieList = document.querySelector("#movie-list");
+    // Create new element for loading movie image
     let movieImage = document.createElement("img");
 
+    // Update movie image link to local image file path
     movieImage.src = movie.image;
+    // Add movie image element to loading region
     movieList.appendChild(movieImage);
 }
